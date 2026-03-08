4889e0ee712b30364c7fb3071df4c55d34d2b369
Chainwalker141 <jmsinjian@gmail.com>
2026-03-08 23:47:22 +0800

    Revert "Removed whitenoise"
    This reverts commit 5157987cce8ca6f2c4520f72fe127490cc09425a.

--- server/server/settings.py
+++ server/server/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,5 +142,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
